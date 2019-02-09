1df8c9cfaf725c6ac9bea9b73fa8aeb03f42cd09
Armaan Kohli <armaankohli@berkeley.edu>
2019-02-09 11:02:27 -0800

    bot with helper functions

---
+++ bot.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+
+# ~~~~~==============   HOW TO RUN   ==============~~~~~
+# 1) Configure things in CONFIGURATION section
+# 2) Change permissions: chmod +x bot.py
+# 3) Run in loop: while true; do ./bot.py; sleep 1; done
+
+from __future__ import print_function
+
+import sys
+import socket
+import json
+
+# ~~~~~============== CONFIGURATION  ==============~~~~~
+# replace REPLACEME with your team name!
+team_name="THEREVENGERS"
+# This variable dictates whether or not the bot is connecting to the prod
+# or test exchange. Be careful with this switch!
+test_mode = True
+
+# This setting changes which test exchange is connected to.
+# 0 is prod-like
+# 1 is slower
+# 2 is empty
+test_exchange_index=2
+prod_exchange_hostname="production"
+
+port=25000 + (test_exchange_index if test_mode else 0)
+exchange_hostname = "test-exch-" + team_name if test_mode else prod_exchange_hostname
+
+# ~~~~~============== NETWORKING CODE ==============~~~~~
+def connect():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((exchange_hostname, port))
+    return s.makefile('rw', 1)
+
+def write_to_exchange(exchange, obj):
+    json.dump(obj, exchange)
+    exchange.write("\n")
+
+def read_from_exchange(exchange):
+    return json.loads(exchange.readline())
+
+def print_from_exchange(exchange):
+    print(read_from_exchange(exchange))
+
+id_num = 0
+
+def add_action(symbol, option, price, size, exchange):
+    global id_num
+    id_num += 1
+    request = {"type": "add", "order_id": id_num, "symbol", symbol,
+    "dir": option, "price": price, "size": size}
+    print(request)
+    write_to_exchange(exchange, request)
+
+def convert_action(exchange, symbol, size, option):
+    global id_num
+    id_num += 1
+    request = {"type": "convert", "order_id", id_num, "symbol": symbol, "dir": option,
+    "size": size}
+    print(request)
+    write_to_exchange(exchange, request)
+
+def cancel_action(exchange):
+    global id_num
+    id_num += 1
+    request = {"type": "cancel", "order_id", id_num}
+    print(request)
+    write_to_exchange(exchange, request)
+
+#write functions for sums, averages, etc
+
+# ~~~~~============== MAIN LOOP ==============~~~~~
+
+def main():
+    exchange = connect()
+    write_to_exchange(exchange, {"type": "hello", "team": team_name.upper()})
+    hello_from_exchange = read_from_exchange(exchange)
+    # A common mistake people make is to call write_to_exchange() > 1
+    # time for every read_from_exchange() response.
+    # Since many write messages generate marketdata, this will cause an
+    # exponential explosion in pending messages. Please, don't do that!
+    print("The exchange replied:", hello_from_exchange, file=sys.stderr)
+
+if __name__ == "__main__":
+    main()
