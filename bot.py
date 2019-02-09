f27d5596d2b2105ced76137d3155c4f738e2d31d
Austin <austinchiang1@berkeley.edu>
2019-02-09 15:16:19 -0800

    austin v4

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ from time import sleep
 
 # ~~~~~============== CONFIGURATION  ==============~~~~~
 # replace REPLACEME with your team name!
-team_name="therevengers"
+team_name="THEREVENGERS"
 # This variable dictates whether or not the bot is connecting to the prod
 # or test exchange. Be careful with this switch!
 test_mode = True
